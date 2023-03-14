fecd75a03d4d11bc17df231450b1bc08628ae5d0
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-03-14 14:38:57 +0530

    changed video path again

--- cocktaildb/src/components/Hero.jsx
+++ cocktaildb/src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero(){
             <div className=" z-30 bg-black bg-opacity-20 absolute h-100v w-full flex justify-center items-center">
                 <p className="text-6xl text-white font-extrabold">Find the best Cocktails</p>
             </div>
-            <video src="src/video/video.mp4" autoPlay loop muted className="object-cover h-100v w-full object-center"/>
+            <video src="/video/video.mp4" autoPlay loop muted className="object-cover h-100v w-full object-center"/>
         </div>
         <div className="text-6xl min-h-screen">iudhfpieufh</div>
         <div className="text-6xl">iudhfpieufh</div>
