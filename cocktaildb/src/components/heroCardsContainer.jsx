0d2a2c9c2aa3d61e0e835ca01315f3b02e7401aa
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-03-15 17:27:47 +0530

    Added Autoplay to the carousel

--- cocktaildb/src/components/heroCardsContainer.jsx
+++ cocktaildb/src/components/heroCardsContainer.jsx
@@ -2,7 +2,7 @@ import { DataContext } from "./data"
 import { useContext } from "react"
 import HeroCard from "./heroCard"
 import {Swiper, SwiperSlide} from "swiper/react"
-import { Navigation, Pagination, Keyboard,EffectCoverflow } from "swiper"
+import { Navigation, Pagination, Keyboard,EffectCoverflow, Autoplay } from "swiper"
 import "swiper/css";
 import "swiper/css/navigation";
 
@@ -16,19 +16,24 @@ function HeroCardsContainer(){
       <Swiper 
                 slidesPerView={3}
               spaceBetween={30}  centeredSlides={true} navigation={true}
-               pagination={{dynamicBullets:true,}} modules={[Navigation,Pagination,Keyboard,EffectCoverflow ]}
+               pagination={{dynamicBullets:true,}}
                keyboard={
                 {enabled:true}
                }
+               autoplay={{
+                delay:1500,
+                disableOnInteraction:false
+               }}
                effect={"coverflow"}
                coverflowEffect={{
-                rotate: 80,
+                rotate: 60,
                 stretch: 0,
                 depth: 100,
                 modifier: 1,
                 slideShadows: true,
               }}
                grabCursor={true}
+               modules={[Navigation,Pagination,Keyboard,EffectCoverflow, Autoplay ]}
                className="flex justify-center items-center">
             {data?.data.drinks.map(drink=><SwiperSlide className="" key={drink.idDrink}><HeroCard image={drink.strDrinkThumb} title={drink.strDrink} key={drink.idDrink} cardId={drink.idDrink}/></SwiperSlide>)}
         </Swiper>
