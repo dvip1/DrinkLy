7d8a19c649383aba325bc1460ee3da532b6212cb
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-03-17 13:15:44 +0530

    Further Error Handling

--- cocktaildb/src/components/search.jsx
+++ cocktaildb/src/components/search.jsx
@@ -32,9 +32,9 @@ function Search(){
             <button  className="border border-black" onClick={refetch}>Search</button>
         </div>  
 <div className="grid esm:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-10">
-        {!isError && data?.drinks?.map((drink,index)=>(<Card image={drink.strDrinkThumb} title={drink.strDrink} key={drink.idDrink} cardId={drink.idDrink} index={index}/> )
+        {!isError && data?.drinks?data.drinks.map((drink,index)=>(<Card image={drink.strDrinkThumb} title={drink.strDrink} key={drink.idDrink} cardId={drink.idDrink} index={index}/> )
 
-)}
+):<p className="text-2xl">Please search a valid Drink name</p>}
        
      </div>
         </div>
