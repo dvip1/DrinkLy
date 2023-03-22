37ac6a7dcd5a1ef3f1001ac6e93e1f9f1b471857
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-03-22 14:34:23 +0530

    Hero background color update

--- cocktaildb/src/components/Hero.jsx
+++ cocktaildb/src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <div>
       <div className="min-h-screen w-100v absolute">
-        <div className=" z-30 bg-black bg-opacity-20 absolute h-100v w-full flex justify-center items-center">
+        <div className=" z-30  absolute h-100v w-full flex justify-center items-center">
           <p className="text-6xl text-white font-extrabold">
             Find the best Cocktails
           </p>
@@ -19,7 +19,7 @@ function Hero() {
           className="object-cover h-100v w-full object-center"
         />
       </div>
-      <div className="text-6xl min-h-screen"></div>
+      <div className="text-6xl min-h-screen bg-black z-50"></div>
 
       <HeroCardsContainer />
       <Categories />
