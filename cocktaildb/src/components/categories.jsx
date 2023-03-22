f594b675b3b7f32d459825cc3def08eae1532338
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-03-22 09:05:25 +0530

    wrapped up with categories section

--- cocktaildb/src/components/categories.jsx
+++ cocktaildb/src/components/categories.jsx
@@ -1,7 +1,11 @@
 import { DataContext } from "./data";
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import Martini from "../components/assets/images/margarita-cocktail.jpg"
+import Margarita from "../components/assets/images/margarita-cocktail.jpg"
+import Martini from "../components/assets/images/martini.webp"
+import Rum from "../components/assets/images/Rum.jpg"
+import Gin from "../components/assets/images/gin.webp"
+import Vodka from "../components/assets/images/vodka.jpg"
 
 function Categories() {
   let [
@@ -20,14 +24,53 @@ function Categories() {
       <div className="min-h-screen w-full bg-black grid esm:grid-cols-2 esm:gap-2 md:grid-cols-3 md:gap-4 esm:p-5 md:p-6">
         <div className=" overflow-hidden relative col-span-2">
           <p className="text-3xl text-white absolute bottom-4 left-2">
-            Martini
+            Margarita
           </p>
           <div className="h-full w-full group">
             <div
               className="h-full w-full  group-hover:scale-125 group-hover:opacity-40 transition-all"
               style={{
                 backgroundImage:
-                 `url(${Martini})`,
+                 `url(${Margarita})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+            <div
+              className="opacity-0 group-hover:opacity-100 absolute h-full w-full
+                          top-0 right-0 bottom-0 left-0 overflow-hidden
+                          flex justify-center items-center
+                         transition-all ease-in-out"
+            >
+              <NavLink
+                to="/popular"
+                onClick={() => {
+                  setLinkInfo((prev) => {
+                    const newInfo = { ...prev, drinkName: "Margarita" };
+                    if (newInfo && newInfo.drinkName === "Margarita") {
+                      refetch();
+                    }
+                    return newInfo;
+                  });
+                }}
+                className="border border-white rounded-lg text-white p-2 hover:bg-white hover:bg-opacity-30"
+              >
+                Know more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className=" overflow-hidden relative">
+          <p className="text-3xl text-white absolute bottom-4 left-2">
+            Martini
+          </p>
+          <div className="h-full w-full group">
+            <div
+              className="h-full w-full  hover:scale-125   group-hover:scale-125 group-hover:opacity-40 transition-all"
+              style={{
+                backgroundImage:
+                  `url(${Martini})`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat",
@@ -57,16 +100,57 @@ function Categories() {
             </div>
           </div>
         </div>
+
         <div className=" overflow-hidden relative">
           <p className="text-3xl text-white absolute bottom-4 left-2">
-            Martini
+            Vodka
           </p>
           <div className="h-full w-full group">
             <div
               className="h-full w-full  hover:scale-125   group-hover:scale-125 group-hover:opacity-40 transition-all"
               style={{
                 backgroundImage:
-                  `url(${Martini})`,
+                  `url(${Vodka})`,
+                backgroundSize: "cover",
+                backgroundPosition: "bottom",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+            <div
+              className="opacity-0 group-hover:opacity-100 absolute h-full w-full
+                          top-0 right-0 bottom-0 left-0 overflow-hidden
+                          flex justify-center items-center
+                         transition-all ease-in-out"
+            >
+              <NavLink
+                to="/popular"
+                onClick={() => {
+                  setLinkInfo((prev) => {
+                    const newInfo = { ...prev, drinkName: "Vodka" };
+                    if (newInfo && newInfo.drinkName === "Vodka") {
+                      refetch();
+                    }
+                    return newInfo;
+                  });
+                }}
+                className="border border-white rounded-lg text-white p-2 hover:bg-white hover:bg-opacity-30"
+              >
+                Know more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+
+        <div className=" overflow-hidden relative">
+          <p className="text-3xl text-white absolute bottom-4 left-2">
+            Gin
+          </p>
+          <div className="h-full w-full group">
+            <div
+              className="h-full w-full  hover:scale-125   group-hover:scale-125 group-hover:opacity-40 transition-all"
+              style={{
+                backgroundImage:
+                  `url(${Gin})`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat",
@@ -99,14 +183,14 @@ function Categories() {
 
         <div className=" overflow-hidden relative">
           <p className="text-3xl text-white absolute bottom-4 left-2">
-            Martini
+            Rum
           </p>
           <div className="h-full w-full group">
             <div
               className="h-full w-full  hover:scale-125   group-hover:scale-125 group-hover:opacity-40 transition-all"
               style={{
                 backgroundImage:
-                  `url(${Martini})`,
+                  `url(${Rum})`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat",
@@ -124,86 +208,6 @@ function Categories() {
                   setLinkInfo((prev) => {
                     const newInfo = { ...prev, drinkName: "Rum" };
                     if (newInfo && newInfo.drinkName === "Rum") {
-                      refetch();
-                    }
-                    return newInfo;
-                  });
-                }}
-                className="border border-white rounded-lg text-white p-2 hover:bg-white hover:bg-opacity-30"
-              >
-                Know more
-              </NavLink>
-            </div>
-          </div>
-        </div>
-
-        <div className=" overflow-hidden relative">
-          <p className="text-3xl text-white absolute bottom-4 left-2">
-            Martini
-          </p>
-          <div className="h-full w-full group">
-            <div
-              className="h-full w-full  hover:scale-125   group-hover:scale-125 group-hover:opacity-40 transition-all"
-              style={{
-                backgroundImage:
-                  `url(${Martini})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-              }}
-            ></div>
-            <div
-              className="opacity-0 group-hover:opacity-100 absolute h-full w-full
-                          top-0 right-0 bottom-0 left-0 overflow-hidden
-                          flex justify-center items-center
-                         transition-all ease-in-out"
-            >
-              <NavLink
-                to="/popular"
-                onClick={() => {
-                  setLinkInfo((prev) => {
-                    const newInfo = { ...prev, drinkName: "Margarita" };
-                    if (newInfo && newInfo.drinkName === "Margarita") {
-                      refetch();
-                    }
-                    return newInfo;
-                  });
-                }}
-                className="border border-white rounded-lg text-white p-2 hover:bg-white hover:bg-opacity-30"
-              >
-                Know more
-              </NavLink>
-            </div>
-          </div>
-        </div>
-
-        <div className=" overflow-hidden relative">
-          <p className="text-3xl text-white absolute bottom-4 left-2">
-            Martini
-          </p>
-          <div className="h-full w-full group">
-            <div
-              className="h-full w-full  hover:scale-125   group-hover:scale-125 group-hover:opacity-40 transition-all"
-              style={{
-                backgroundImage:
-                  `url(${Martini})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-              }}
-            ></div>
-            <div
-              className="opacity-0 group-hover:opacity-100 absolute h-full w-full
-                          top-0 right-0 bottom-0 left-0 overflow-hidden
-                          flex justify-center items-center
-                         transition-all ease-in-out"
-            >
-              <NavLink
-                to="/popular"
-                onClick={() => {
-                  setLinkInfo((prev) => {
-                    const newInfo = { ...prev, drinkName: "Brandy" };
-                    if (newInfo && newInfo.drinkName === "Brandy") {
                       refetch();
                     }
                     return newInfo;
