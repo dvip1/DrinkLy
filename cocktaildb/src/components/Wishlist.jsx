be48390304ac7bc93559a8044f4f3aabb7919811
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-03-23 18:05:40 +0530

    Added delete button to Wishlist

--- cocktaildb/src/components/Wishlist.jsx
+++ cocktaildb/src/components/Wishlist.jsx
@@ -1,22 +1,36 @@
 import { userDataContext } from "./userData/userData";
 import { useContext } from "react";
 
-let Wishtlist=()=>{
-    let [wishlistData, setWishlistData, cartData, setCartData] =
+let Wishtlist = () => {
+  let [wishlistData, setWishlistData, cartData, setCartData] =
     useContext(userDataContext);
 
-    return (
-        <div className="grid grid-cols-6" key={wishlistData.id}>
-            {wishlistData.map((drink)=>{
-                return (
-                   <div className="">
-                    <div className="">
-                        <img src={`${drink.image}`} className="w-[22vw]"></img>
-                        <p className="text-2xl text-center">{drink.name}</p>
-                    </div></div> 
-                )
-            })}
-        </div>
-    )
-}
-export default Wishtlist
+  let handleDelete = (deleteWishlistId) => {
+    setWishlistData((prev) =>
+      prev.filter((drink) => {
+        if (drink.id !== deleteWishlistId) {
+          return true;
+        }
+      })
+    );
+  };
+
+  return (
+    <div className="grid grid-cols-6" key={wishlistData.id}>
+      {wishlistData.map((drink) => {
+        return (
+          <div className="">
+            <div className="">
+              <img src={`${drink.image}`} className="w-[22vw]"></img>
+              <p className="text-2xl text-center">{drink.name}</p>
+              <button onClick={() => handleDelete(drink.id)} className="border">
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Wishtlist;
